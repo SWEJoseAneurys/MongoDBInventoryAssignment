304c70ebca45e3ec54dd7264f2f89554f096d3b9
Jose Castro <JoseAneurys@me.com>
2023-01-20 19:26:28 -0500

    Connecting database to server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 
 const app = express();
 
 app.use(express.static('public'));
+
+const connectionString = `mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.PASSWORD}@mongosetupcluster.li7admh.mongodb.net/Inventory?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(connectionString, {
+    
+})
+
 
 app.get('/', (req,res) => {
     console.log("Welcome to the homepage!")
